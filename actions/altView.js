732adc2971e50fafd68bdd7d11972d46f44eb239
Ken <aenkil@gmail.com>
2015-11-19 08:53:11 +1100

    For the horde 2

--- actions/altView.js
+++ actions/altView.js
@@ -1,13 +1,15 @@
 // Assume container level component
 // Also assume all dependencies are imported
+import TopicAction from 'actions/TopicActions';
+
 class EntryBox extends React.Component {
 
   render() {
     return (
       <div className={styles.entrybox}>
         <TopicTextInput 
-          onChange={text => typing(text)} 
-          onSave={text => createTopic(text)} />
+          onChange={text => TopicAction.typing(text)} 
+          onSave={text => TopicAction.createTopic(text)} />
       </div>
     );
   }
